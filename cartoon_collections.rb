5344cf4f1dffecf26011c1d013ae0f14e16cd774
Daniel Reina <dannyflatiron@gmail.com>
2019-11-10 23:54:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,9 +6,10 @@ end
 
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map { |elements| }
-    elements.capitalize
+  planeteer_calls.map do |elements|
+    elements.capitalize + "?"
     # binding.pry
+  end
 end
 
 def long_planeteer_calls# code an argument here
