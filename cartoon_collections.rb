27e720b297994e5112e433ff97070ef8e9b8ea49
Daniel Reina <dannyflatiron@gmail.com>
2019-11-11 00:13:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls)
-  calls.any? { |elements| elements.length = 4 }
+  calls.any? { |elements| elements.length > 4 }
 end
 
 def find_the_cheese# code an argument here
