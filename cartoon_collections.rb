26b166da0d2d96261bc20d119580fc0043035731
Daniel Reina <dannyflatiron@gmail.com>
2019-11-11 00:30:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,7 @@ end
 
 def find_the_cheese(snacks)
   # cheese_types = ["cheddar", "gouda", "camembert"]
-  if snacks.include? 'cheddar' || 'gouda' || 'camembert'
+  if snacks.include?('cheddar' || 'gouda' || 'camembert')
     puts "cheddar"
   else
     nil
